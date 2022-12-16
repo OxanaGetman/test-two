11676f9d1f592f0f26eaff9ac81e3b43e9c9539a
unknown <getmanoa@gmail.com>
2022-12-16 15:22:56 +0300

    your message

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-gsap.from(".girl", {y: 900,  duration: 10, delay: 4, ease: "slow(0.7, 0.7, false)", opacity:0});
+gsap.from(".girl", {y: 900,  duration: 10, delay: 2, ease: "slow(0.7, 0.7, false)", opacity:0});
